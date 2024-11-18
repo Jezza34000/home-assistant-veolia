5a9304182b5ec59d30c26da4d580ad04917272ec
Jezza34000 <57314417+Jezza34000@users.noreply.github.com>
2024-11-18 18:30:53 +0100

    refactor _async_update_data TRY300

--- custom_components/veolia/coordinator.py
+++ custom_components/veolia/coordinator.py
@@ -7,6 +7,7 @@ from typing import TYPE_CHECKING
 
 from veolia_api import VeoliaAPI
 from veolia_api.exceptions import VeoliaAPIError
+from veolia_api.model import VeoliaAccountData
 from veolia_api.veolia_api import ConsumptionType
 
 from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
@@ -14,13 +15,10 @@ from homeassistant.exceptions import ConfigEntryAuthFailed
 from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 
 from .const import DOMAIN, LOGGER
+from .data import VeoliaConfigEntry
 
 if TYPE_CHECKING:
-    from veolia_api.model import VeoliaAccountData
-
     from homeassistant.core import HomeAssistant
-
-    from .data import VeoliaConfigEntry
 
 
 class VeoliaDataUpdateCoordinator(DataUpdateCoordinator):
@@ -41,23 +39,21 @@ class VeoliaDataUpdateCoordinator(DataUpdateCoordinator):
         )
         LOGGER.debug("Initializing client VeoliaAPI")
 
-        """Initialize."""
         self.api = VeoliaAPI(
             username=self.config_entry.data[CONF_USERNAME],
             password=self.config_entry.data[CONF_PASSWORD],
         )
 
-
-async def _async_update_data(self) -> VeoliaAccountData:
-    """Update data via library."""
-    try:
-        now = datetime.now()
-        LOGGER.debug(
-            f"Fetching consumption data for {ConsumptionType.MONTHLY.value} "
-            f"Year:{now.year} Month:{now.month}",
-        )
-        await self.api.fetch_all_data(now.year, now.month)
-    except VeoliaAPIError as exception:
-        raise ConfigEntryAuthFailed(exception) from exception
-    else:
-        return self.api.account_data
+    async def _async_update_data(self) -> VeoliaAccountData:
+        """Update data via library."""
+        try:
+            now = datetime.now()
+            LOGGER.debug(
+                f"Fetching consumption data for {ConsumptionType.MONTHLY.value} "
+                f"Year:{now.year} Month:{now.month}",
+            )
+            await self.api.fetch_all_data(now.year, now.month)
+        except VeoliaAPIError as exception:
+            raise ConfigEntryAuthFailed(exception) from exception
+        else:
+            return self.api.account_data
