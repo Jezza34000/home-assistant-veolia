fc91815164b46b11fbe200dafb5c1d40a6666f42
Jezza34000 <57314417+Jezza34000@users.noreply.github.com>
2024-11-18 18:30:26 +0100

    fix D100

--- setup.py
+++ setup.py
@@ -1,7 +1,9 @@
-from setuptools import setup, find_packages
+"""Setup for Veolia."""
+
+from setuptools import find_packages, setup
 
 setup(
     name="veolia",
     version="1",
-    packages=find_packages(include=['custom_components']),
+    packages=find_packages(include=["custom_components"]),
 )
