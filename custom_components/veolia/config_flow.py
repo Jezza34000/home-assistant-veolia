6835ee70a5b5a760b59721285a9848cf73dc8150
Jezza34000 <57314417+Jezza34000@users.noreply.github.com>
2024-11-19 17:25:34 +0100

    feat: improve config_flow for more detailled information on auth fail

--- custom_components/veolia/config_flow.py
+++ custom_components/veolia/config_flow.py
@@ -1,14 +1,13 @@
 """Adds config flow for Veolia."""
 
 from veolia_api import VeoliaAPI
-from veolia_api.exceptions import VeoliaAPIError
+from veolia_api.exceptions import VeoliaAPIInvalidCredentialsError
 import voluptuous as vol
 
 from homeassistant import config_entries
 from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
 
 from .const import DOMAIN
-
 
 class VeoliaFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
     """Config flow for veolia."""
@@ -25,16 +24,19 @@ class VeoliaFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
         self._errors = {}
 
         if user_input is not None:
-            valid = await self._test_credentials(
-                user_input[CONF_USERNAME],
-                user_input[CONF_PASSWORD],
-            )
-            if valid:
-                return self.async_create_entry(
-                    title=user_input[CONF_USERNAME],
-                    data=user_input,
-                )
-            self._errors["base"] = "auth"
+            try:
+                api = VeoliaAPI(user_input[CONF_USERNAME], user_input[CONF_PASSWORD])
+                valid = await api.login()
+
+                if valid:
+                    return self.async_create_entry(
+                        title=user_input[CONF_USERNAME],
+                        data=user_input,
+                    )
+            except VeoliaAPIInvalidCredentialsError:
+                self._errors["base"] = "invalid_credentials"
+            except Exception:
+                self._errors["base"] = "unknown"
 
             return await self._show_config_form(user_input)
         return await self._show_config_form(user_input)
@@ -49,12 +51,3 @@ class VeoliaFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
             errors=self._errors,
         )
 
-    @staticmethod
-    async def _test_credentials(username: str, password: str) -> bool:
-        """Return true if credentials is valid."""
-        try:
-            api = VeoliaAPI(username, password)
-            return await api.login()
-        except VeoliaAPIError:
-            pass
-        return False
